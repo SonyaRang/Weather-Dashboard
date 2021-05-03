e831544c80dc34ae9806d45705efacd32ee8ea57
Sonz <sonyarangraj@msn.com>
2021-05-02 23:32:05 -0500

    added changes

---
+++ assets/weather.js
@@ -0,0 +1,12 @@
+//varibles
+var city="";
+
+var searchBtton = $("#search-button");
+var searchCity = $("#search-thecity");
+var clearBtton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentUvindex= $("#uv-index");
+var WSpeed=$("#wind-speed");
+var Humidty= $("#humidity");
+var currentTemp = $("#temperature");
+var sCity=[];
